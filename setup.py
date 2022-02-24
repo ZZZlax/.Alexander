7d353bfa9ecab5ffda53de792c3bafd7b3e9e6a8
ZZZlax <93495092+ZZZlax@users.noreply.github.com>
2022-02-23 19:34:16 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup_info = dict(
+    name='Alexander',
+    version='1.0',
+    author='ZZZlax',
+    author_email='theoriginalashketchum@protonmail.com',
+    url='https://github.com/ZZZlax/.Alex',
+    download_url='https://github.com/ZZZlax/.Alex',
+    description='Alexander',
+    keywords = ["Alex", "Alexander"],
+    long_description='Translate and search web and wiki with tts module',
+    classifiers=[
+        "Topic :: Utilities",
+        'Development Status :: Production/Stable',
+        'Environment :: X11 Applications',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=['googlesearch-python', 'wikipedia', 'speechrecognition', 'pyttsx3', 'gtts', 'googletrans==4.0.0-rc1'],
+)
+
+setup(**setup_info)
